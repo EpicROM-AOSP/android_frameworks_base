394b206c0a70561cdbc5897974911f676f7806b1
Lars Greiss <kufikugel@googlemail.com>
2016-03-26 21:18:14 -0400

    HeadsUp: add timeout option (1/2) Forward ported to marshmallow/nougat By: @BeansTown106 this was basically a rewrite as everything about it changed
    Change-Id: I9c88d67d25440dff8f545e1330da672be5d36913
    Signed-off-by: mydongistiny <jaysonedson@gmail.com>
    Signed-off-by: xyyx <xyyx@mail.ru>
    Signed-off-by: Arghya Chanda <arghyac35@gmail.com>
    Signed-off-by: SagarMakhar <sagarmakhar@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6292,6 +6292,13 @@ public final class Settings {
          */
         @Readable
         public static final String HEADS_UP_BLACKLIST_VALUES = "heads_up_blacklist_values";
+		
+		/**
+         * Whether to enable status and navigation bar color in battery saver mode.
+         * Heads up timeout configuration
+         * @hide
+         */
+        public static final String HEADS_UP_TIMEOUT = "heads_up_timeout";
 
         /**
          * Keys we no longer back up under the current schema, but want to continue to
