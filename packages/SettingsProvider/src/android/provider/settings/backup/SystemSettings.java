d39b8f288b90704ba9acbf5a5ed31d8d38ecbab9
LuK1337 <priv.luk@gmail.com>
2019-09-20 18:30:03 +0200

    base: Add navbar layout inversion tuning [1/2]
    * Originally implemented by jesec but since
      then has been reworked by me to use RTL
      instead of replacing left / right buttons.
    
    idoybh:
    * Use existing settings observer instead of tunables
    * Properly add the setting to Settings.java So we can back it up
    * Get rid of useless global var
    
    @jhonboy121:
    * use NavigationBarModeController to update layout on settings change
    
    Change-Id: I6159f0232ba2ee9f45d2afbba7d11072d902d9ac
    Co-authored-by: Ido Ben-Hur <idoybh2@gmail.com>
    Signed-off-by: jhonboy121 <alfredmathew05@gmail.com>

--- packages/SettingsProvider/src/android/provider/settings/backup/SystemSettings.java
+++ packages/SettingsProvider/src/android/provider/settings/backup/SystemSettings.java
@@ -89,5 +89,6 @@ public class SystemSettings {
         Settings.System.NOTIFICATION_LIGHT_PULSE,
         Settings.System.COLOR_BUCKET_OVERLAY,
         Settings.System.USE_OLD_MOBILETYPE,
+        Settings.System.NAVIGATION_BAR_INVERSE,
     };
 }
