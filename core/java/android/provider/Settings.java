66be06a5609d945d5e90a01d62bb97d6ae1cfe4a
Anushek Prasal <anushekprasal@gmail.com>
2020-01-03 00:18:59 +0530

    base: Add Touch HIDL support All credits to LineageOS and related authors on: https://github.com/LineageOS/android_lineage-sdk
    Ported to AOSP from lineage-sdk by SKULSHADY
    
    Change-Id: Iac8dba8b2d509fe1fa707efc4da9c661f1321d9d

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5760,6 +5760,12 @@ public final class Settings {
          * @hide
          */
         public static final String COLOR_BUCKET_OVERLAY = "color_bucket_overlay";
+		
+		/**
+         * Whether or not to vibrate when a touchscreen gesture is detected
+         * @hide
+         */
+        public static final String TOUCHSCREEN_GESTURE_HAPTIC_FEEDBACK = "touchscreen_gesture_haptic_feedback";
 
         /**
          * Keys we no longer back up under the current schema, but want to continue to
@@ -5889,6 +5895,7 @@ public final class Settings {
             PRIVATE_SETTINGS.add(DOUBLE_TAP_SLEEP_LOCKSCREEN);
             PRIVATE_SETTINGS.add(DOUBLE_TAP_SLEEP_GESTURE);
             PRIVATE_SETTINGS.add(COLOR_BUCKET_OVERLAY);
+            PRIVATE_SETTINGS.add(TOUCHSCREEN_GESTURE_HAPTIC_FEEDBACK);
         }
 
         /**
