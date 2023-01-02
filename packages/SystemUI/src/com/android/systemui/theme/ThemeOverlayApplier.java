66f7f5feb5e4416b5dc3e18e559314c22c408038
El Dainosor <eldainosor@gmail.com>
2023-01-02 17:38:11 -0300

    base: Add support for Lockscreen clock fonts
    After r16, a commit to cleanup the KeyguardClockSwtich and unify it's structure was merged.
    Also, this added a new resource into the frameworks core, which allows us to overlay the custom clock font.
    So, we need to overlays now to customize this. This makes me really happy because it removes the ugly method that we were doing years ago.
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- packages/SystemUI/src/com/android/systemui/theme/ThemeOverlayApplier.java
+++ packages/SystemUI/src/com/android/systemui/theme/ThemeOverlayApplier.java
@@ -113,6 +113,9 @@ public class ThemeOverlayApplier implements Dumpable {
     @VisibleForTesting
     static final String OVERLAY_CATEGORY_NAVBAR =
             "android.theme.customization.navbar";
+    @VisibleForTesting
+    static final String OVERLAY_CATEGORY_LOCK_CLOCK_FONT =
+            "android.theme.customization.lockscreen_clock_font";
 
     /*
      * All theme customization categories used by the system, in order that they should be applied,
@@ -129,7 +132,8 @@ public class ThemeOverlayApplier implements Dumpable {
             OVERLAY_CATEGORY_ICON_SYSUI,
             OVERLAY_CATEGORY_ICON_SETTINGS,
             OVERLAY_CATEGORY_ICON_THEME_PICKER,
-            OVERLAY_CATEGORY_NAVBAR);
+            OVERLAY_CATEGORY_NAVBAR,
+            OVERLAY_CATEGORY_LOCK_CLOCK_FONT);
 
     /* Categories that need to be applied to the current user as well as the system user. */
     @VisibleForTesting
@@ -141,7 +145,8 @@ public class ThemeOverlayApplier implements Dumpable {
             OVERLAY_CATEGORY_SHAPE,
             OVERLAY_CATEGORY_ICON_ANDROID,
             OVERLAY_CATEGORY_ICON_SYSUI,
-            OVERLAY_CATEGORY_NAVBAR);
+            OVERLAY_CATEGORY_NAVBAR,
+            OVERLAY_CATEGORY_LOCK_CLOCK_FONT);
 
     /* Allowed overlay categories for each target package. */
     private final Map<String, Set<String>> mTargetPackageToCategories = new ArrayMap<>();
@@ -188,6 +193,7 @@ public class ThemeOverlayApplier implements Dumpable {
         mCategoryToTargetPackage.put(OVERLAY_CATEGORY_ICON_LAUNCHER, mLauncherPackage);
         mCategoryToTargetPackage.put(OVERLAY_CATEGORY_ICON_THEME_PICKER, mThemePickerPackage);
         mCategoryToTargetPackage.put(OVERLAY_CATEGORY_NAVBAR, SYSUI_PACKAGE);
+        mCategoryToTargetPackage.put(OVERLAY_CATEGORY_LOCK_CLOCK_FONT, ANDROID_PACKAGE);
 
         dumpManager.registerDumpable(TAG, this);
     }
