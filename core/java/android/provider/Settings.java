648e5b9a04a537f6d016c4bc4a5d3da1e7619c2a
escobar1945 <candaroglu197878@gmail.com>
2023-09-10 22:04:17 +0300

    Revert "SystemUI: Add support for GameSpace" This reverts commit f393f6bf490515c4fcfbc99580db7e65f645757d

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6070,20 +6070,6 @@ public final class Settings {
         public static final String RETICKER_COLORED = "reticker_colored";
 
         /**
-         * GameSpace: List of added games by user
-         * @hide
-         */
-        @Readable
-        public static final String GAMESPACE_GAME_LIST = "gamespace_game_list";
-
-        /**
-         * GameSpace: Whether fullscreen intent will be suppressed while in game session
-         * @hide
-         */
-        @Readable
-        public static final String GAMESPACE_SUPPRESS_FULLSCREEN_INTENT = "gamespace_suppress_fullscreen_intent";
-
-        /**
          * boolean value. toggles using arrow key locations on nav bar
          * as left and right dpad keys
          * @hide
